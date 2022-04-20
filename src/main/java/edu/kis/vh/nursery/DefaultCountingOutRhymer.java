4b8c783dc070daae9a32f3980ce49824ad03131c
Jakub Więcek <71319403+Kubator00@users.noreply.github.com>
2022-04-20 19:26:10 +0200

    11. usuwanie nieużywanych setterów

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -39,8 +39,5 @@ public class DefaultCountingOutRhymer {
     public int getTotal() { 
         return total;
     }
-
-    public void setTotal(int total) {
-        this.total = total;
-    }
+    
 }
