52c227ec3b0d64ae76497477287b19033602eb4a
Jakub Więcek <71319403+Kubator00@users.noreply.github.com>
2022-04-20 19:11:07 +0200

    8. zmiana atrybutów klas pod kątem widoczności

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -3,7 +3,7 @@ package edu.kis.vh.nursery;
 //wiersze źle sformatowane: 5,12,14,15
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-    int totalRejected = 0;
+    private int totalRejected = 0;
 
     public int reportRejected() {
         return totalRejected;
