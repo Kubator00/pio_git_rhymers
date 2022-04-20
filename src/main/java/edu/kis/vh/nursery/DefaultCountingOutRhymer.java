96c2998768f0d16812746b3083ca3b4cc7c7f53d
235945 <235945@CARBON.CTI.P.LODZ.PL>
2022-04-20 13:22:02 +0200

    5. Zamiana liczb na stałe

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,14 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int ARRAY_CAPACITY = 12;
+    public static final int ARRAY_CHECK = -1;
+    public static final int ARRAY_FULL = 11;
 
-    public int total = -1;
+    private int[] numbers = new int[ARRAY_CAPACITY];
+
+    public int total = ARRAY_CHECK;
+
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +17,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == ARRAY_CHECK;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == ARRAY_FULL;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return ARRAY_CHECK;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return ARRAY_CHECK;
         return numbers[total--];
     }
 
