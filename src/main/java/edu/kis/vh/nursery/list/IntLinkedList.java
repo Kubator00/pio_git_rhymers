2db72dea4fd4903f20e05e2e299ef97e971f5625
Jakub Więcek <71319403+Kubator00@users.noreply.github.com>
2022-04-28 21:09:06 +0200

    13. przeorganizowanie projektu, zmiana klasy na klase inner

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,41 +2,70 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
-	private static final int EMPTY_LIST = -1;
-	private Node last;
-	private int i;
+    static class Node {
 
-	public void push(int i) {
-		if (last == null)
-			last = new Node(i);
-		else {
-			last.next = new Node(i);
-			last.next.setPrev(last);
-			last = last.next;
-		}
-	}
+        private int value;
+        private Node prev;
+        public Node next;
 
-	public boolean isEmpty() {
-		return last == null;
-	}
+        public Node(int i) {
+            setValue(i);
+        }
 
-	public boolean isFull() {
-		return false;
-	}
+        public int getValue() {
+            return value;
+        }
 
-	public int top() {
-		if (isEmpty())
-			return EMPTY_LIST;
-		return last.getValue();
-	}
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+    }
 
 
-	public int pop() {
-		if (isEmpty())
-			return EMPTY_LIST;
-		int ret = last.getValue();
-		last = last.getPrev();
-		return ret;
-	}
+    private static final int EMPTY_LIST = -1;
+    private Node last;
+    private int i;
+
+
+    public void push(int i) {
+        if (last == null)
+            last = new Node(i);
+        else {
+            last.next = new Node(i);
+            last.next.setPrev(last);
+            last = last.next;
+        }
+    }
+
+    public boolean isEmpty() {
+        return last == null;
+    }
+
+    public boolean isFull() {
+        return false;
+    }
+
+    public int top() {
+        if (isEmpty())
+            return EMPTY_LIST;
+        return last.getValue();
+    }
+
+
+    public int pop() {
+        if (isEmpty())
+            return EMPTY_LIST;
+        int ret = last.getValue();
+        last = last.getPrev();
+        return ret;
+    }
 
 }
